016f79653f0422bd8c8c165d96adf82b330aa2c1
sauasmast <sdahal@caldwell.edu>
2019-02-23 16:40:03 -0500

    Last try

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('./dist/udemyPractice'));
 
 app.get('/*', function(req, res) {
-  res.sendFile('./dist/udemyPractice/index.html');
+  res.sendFile(path.join(__dirname + '/dist/udemyPractice/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
